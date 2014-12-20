d4162a652fb1e12beb43256678724b7f31d7683d
Max Loginov <walkindude@Maxs-MBP.Dlink>
2014-12-20 10:00:06 +0300

    Добавлена библиотека для парсинга Excel-файлов

--- tmpl/default.php
+++ tmpl/default.php
@@ -6,6 +6,75 @@ JHtml::script('d3.min.js', 'modules/mod_grables/js/');
 
 JHtml::script('grables.js', 'modules/mod_grables/js/');
 
+define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+
+require_once('modules/mod_grables/Classes/PHPExcel/IOFactory.php');
+
+$objPHPExcel = PHPExcel_IOFactory::load("modules/mod_grables/test.xlsx");
+
+// все данные в первом листе
+$worksheet = $objPHPExcel->getActiveSheet();
+
+$columnWithValue = -1;	
+
+$grables = [];
+$grablesCell;
+
+$currentCell = [1];
+
+foreach ($worksheet->getRowIterator() as $row) {
+	$cellIterator = $row->getCellIterator();
+	$cellIterator->setIterateOnlyExistingCells(true);
+	
+	$i = 0;
+	$cellVal;
+	
+	$grablesCell = [];
+	$newCell = [];
+	$cellToAdd = false;
+	
+	foreach ($cellIterator as $cell) {
+		if (!is_null($cell)) {
+			$cellVal = $cell->getCalculatedValue();
+			
+			if ($columnWithValue < 0 && $cellVal === "Исполнено бюджет субъекта РФ") {
+				$columnWithValue = $i;
+			}
+			
+			switch ($i) {
+				case 0 :
+					if ($cellVal === implode(".", $currentCell)) {
+						$cellToAdd = true;
+						$currentCell[count($currentCell) - 1] += 1; 
+					}
+					break;
+					
+				case 1 :	
+					if ($cellToAdd) {
+						$newCell["name"] = $cellVal;
+						$newCell["level"] = "1";
+						$newCell["children"] = [];
+					}
+					break;
+					
+				case $columnWithValue : 
+					if ($cellToAdd) {
+						$newCell["value"] = floatval($cellVal); 
+					}
+					break;
+			}
+		}
+		
+		$i += 1;
+	}
+	
+	if ($cellToAdd) {
+		$grables[] = $newCell;
+	}
+}
+
+echo print_r($grables);
+
 function escapeJsonString($value) {
     # list from www.json.org: (\b backspace, \f formfeed)    
     $escapers =     array("\\",  "/",   "\"",  "\n",  "\r",  "\t", "\x08", "\x0c");
